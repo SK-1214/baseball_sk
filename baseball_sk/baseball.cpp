21b874d36c68e842a031ff791b599b6e7b5597d6
sk.lee <moonstar11430@gmail.com>
2025-07-02 13:49:11 +0900

    1st commit

--- baseball_sk/baseball.cpp
+++ baseball_sk/baseball.cpp
@@ -1,3 +1,10 @@
-class BaseBall {
+#include <stdexcept>
 
+using namespace std;
+
+class Baseball {
+public:
+	void guess(const string& string) {
+		throw length_error("Must be three letters");
+	}
 };
--- baseball_sk/main.cpp
+++ baseball_sk/main.cpp
@@ -1,13 +1,9 @@
 #include "gmock/gmock.h"
 #include "baseball.cpp"
 
-TEST(BaseballGame, TryGame) {
-	EXPECT_EQ(1, 1);
-	EXPECT_EQ(1, 1);
-	EXPECT_EQ(1, 1);
-	EXPECT_EQ(2, 2);
-	EXPECT_EQ(2, 2);
-	EXPECT_EQ(2, 2);
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatch) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
 }
 
 int main() {
