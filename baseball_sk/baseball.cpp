73636c0204e37e396b35e6efbac378c93e9edb1f
sk.lee <moonstar11430@gmail.com>
2025-07-02 14:08:46 +0900

    check argument

--- baseball_sk/baseball.cpp
+++ baseball_sk/baseball.cpp
@@ -4,7 +4,29 @@ using namespace std;
 
 class Baseball {
 public:
-	void guess(const string& string) {
-		throw length_error("Must be three letters");
+	void guess(const string& guessNumber) {
+		assertIllegalArgument(guessNumber);
+	}
+	void assertIllegalArgument(const std::string& guessNumber)
+	{
+		if (guessNumber.length() != 3) {
+			throw length_error("Must be three letters");
+		}
+
+		for (char ch : guessNumber) {
+			if (ch >= '0' && ch <= '9') continue;
+
+			throw invalid_argument("must be number");
+		}
+
+		if (IsDuplicateNumber(guessNumber)) {
+			throw invalid_argument("same number");
+		}
+	}
+	bool IsDuplicateNumber(const std::string& guessNumber)
+	{
+		return guessNumber[0] == guessNumber[1] ||
+			guessNumber[0] == guessNumber[2] ||
+			guessNumber[2] == guessNumber[1];
 	}
 };
--- baseball_sk/main.cpp
+++ baseball_sk/main.cpp
@@ -1,9 +1,24 @@
 #include "gmock/gmock.h"
 #include "baseball.cpp"
 
-TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatch) {
+class BaseballFixture : public testing::Test {
+public:
 	Baseball game;
-	EXPECT_THROW(game.guess(string("12")), length_error);
+	void assertIllegalArgument(string guessNumber) {
+		try {
+			game.guess(guessNumber);
+			FAIL();			
+		}
+		catch(exception e){
+
+		}
+	}
+};
+
+TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase) {
+	assertIllegalArgument("12");
+	assertIllegalArgument("12s");
+	assertIllegalArgument("121");
 }
 
 int main() {
